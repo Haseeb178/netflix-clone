6340b074057ed47b365bd7469c4ae4a0e4cc649e
Haseeb Ahmed <128047743+Haseeb178@users.noreply.github.com>
2025-06-27 19:44:16 +0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import Navbar from "@/Componenets/Navbar";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
